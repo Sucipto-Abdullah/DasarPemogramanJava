66e543a9d898f55c7f6a8f6bcb2d8cf3bbcc7412
Sucipto-Abdullah <ciptoabdul23@gmail.com>
2025-09-30 15:35:57 +0700

    push

---
+++ Praktikum05/testHewan.java
@@ -0,0 +1,13 @@
+class testHewan {
+
+    public static void main(String[] args) {
+        
+        // System.out.println(kucing.getNama());
+
+        Kucing kucing1 = new Kucing();
+
+        kucing1.berkomunikasi();
+
+    }
+    
+}
